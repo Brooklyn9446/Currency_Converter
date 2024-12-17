787250922e9884982735cf0bfa12ab2331004f46
Brooklyn9946 <niranjanskailas@gmail.com>
2024-12-17 23:07:18 +0530

    feature1

--- script.js
+++ script.js
@@ -1,9 +1,16 @@
-let a= document.body.querySelector(".selector")
+let a= document.body.querySelectorAll(".selector")
 let data=[...a];
 
 
-data.forEach(element => {
-    addEventListener('click',()=>{
-        console.log(element.innerText)
+
+for( let select of data){
+    console.log(select)
+    for(country in countryList){
+        let option=document.createElement('option');
+        option.innerText=country;
+        select.append(option)
+    }
+    select.addEventListener("change",(event)=>{
+        console.log(event.target.value)
     })
-});
+}
